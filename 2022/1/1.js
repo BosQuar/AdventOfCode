f8ee1e3f6e950300bc4fa21f8ad8d874aee9a12a
Oskar <87418203+OskarWehype@users.noreply.github.com>
2022-12-02 22:31:24 +0100

    dos problemas dun and go init

--- 2022/1/1.js
+++ 2022/1/1.js
@@ -20,25 +20,27 @@ try {
   console.error(err)
 }
 
-
 let sum = 0;
+let maxValue = 0
 
 array.forEach((string) => {
-  if (string) {
+  if (string.length !== 0) {
     sum += parseInt(string, 10);
   } else {
     summedArray.push(sum);
+    if (sum > maxValue){
+      maxValue = sum
+    }
     sum = 0;
   }
 });
 
-const maxValue = Math.max(...summedArray);
-console.log(maxValue);
+console.log(maxValue)
 
 const sumTopThree = summedArray
   .sort((a, b) => a - b)
   .reverse()
   .slice(0, 3)
-  .reduce((a, b) => b + a, 0);
+  .reduce((value, sum) => sum + value, 0);
 
 console.log(sumTopThree);
